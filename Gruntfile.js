2858446d8f902590b1e88874bb522fae350716b0
ngryman <ngryman@gmail.com>
2013-05-19 11:27:50 -0400

    fixed compass error files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
 			all: {
 				options: {
 					banner: '<%= meta.banner %>\n',
-					basepath: 'app/scripts',
+					basepath: 'app/scripts'
 				},
 				src: 'app/scripts/**/*.js',
 				dest: 'app/app.js'
