4d812394ce9351c10d9beff7fafdf0ea819cc432
Tuizi <fabien.rogeret@gmail.com>
2013-05-21 19:28:11 -0400

    Header menu
    Place items in header menu
    
    -Place items in header
    -Header cursor
    
    Header menu
    
    Add right arrow for notification box
    Increase left value of magnifier and righ value to Profile button to be conform to UX
    
    Rebase fix for pullrequest
    
    Header menu
    
    Place items in header menu
    
    Header menu
    
    -Place items in header
    -Header cursor
    
    Header menu
    
    Add right arrow for notification box
    Increase left value of magnifier and righ value to Profile button to be conform to UX

--- app/app.js
+++ app/app.js
@@ -1,12 +1,29 @@
 /*! gps-proto - v0.0.2 - 2013-05-25
 * Copyright (c) 2013 ; Licensed GPS */
 
-(function(){function require(e,t){for(var n=[],r=e.split("/"),i,s,o=0;(s=r[o++])!=null;)".."==s?n.pop():"."!=s&&n.push(s);n=n.join("/"),o=require,s=o.m[t||0],i=s[n+".js"]||s[n+"/index.js"]||s[n],r='Cannot require("'+n+'")';if(!i)throw Error(r);if(s=i.c)i=o.m[t=s][e=i.m];if(!i)throw Error(r);return i.exports||i(i,i.exports={},function(n){return o("."!=n.charAt(0)?n:e+"/../"+n,t)}),i.exports};
+(function(){function require(e,t){for(var n=[],r=e.split("/"),i,s,o=0;s=r[o++];)".."==s?n.pop():"."!=s&&n.push(s);n=n.join("/"),o=require,s=o.m[t||0],i=s[n+".js"]||s[n+"/index.js"]||s[n],r='Cannot require("'+n+'")';if(!i)throw Error(r);if(s=i.c)i=o.m[t=s][e=i.m];if(!i)throw Error(r);return i.exports||i(i,i.exports={},function(n){return o("."!=n.charAt(0)?n:e+"/../"+n,t)}),i.exports};
 require.m = [];
-require.m[0] = { "app.js": function(module, exports, require){'use strict';
+require.m[0] = { "app/scripts/app.js": function(module, exports, require){'use strict';
 
 $('main').on('tap', '.challenge .stats, .challenge .close', function() {
-	$(this).closest('.challenge').toggleClass('is-details');
-});}};
-gps = require('app.js');
+    $(this).closest('.challenge').toggleClass('is-details');
+});
+
+(function headerCursorBehavior() {
+    var $navCursor = $('.nav-cursor'),
+        $navItems = $('.nav-item');
+
+    $navItems.on('tap', function() {
+        var $this = $(this);
+
+        $navItems.removeClass('is-selected');
+        $this.addClass('is-selected');
+
+        var pos = $this.position().left + ($this.outerWidth() - $this.closest('nav').innerWidth()) / 2;
+        $navCursor.css('transform', 'translateX(' + pos + 'px)');
+    });
+})();
+
+}};
+undefined = require('index.js');
 }());
