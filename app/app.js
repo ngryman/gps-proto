77c9a5eeece4ef3029f37bbdf8798a03fadc7906
Nicolas Gryman <ngryman@gmail.com>
2013-05-24 00:21:55 -0400

    bump version to 0.0.2.

---
+++ app/app.js
@@ -0,0 +1,12 @@
+/*! gps-proto - v0.0.1 - 2013-05-24
+* Copyright (c) 2013 ; Licensed GPS */
+
+(function(){function require(e,t){for(var n=[],r=e.split("/"),i,s,o=0;s=r[o++];)".."==s?n.pop():"."!=s&&n.push(s);n=n.join("/"),o=require,s=o.m[t||0],i=s[n+".js"]||s[n+"/index.js"]||s[n],r='Cannot require("'+n+'")';if(!i)throw Error(r);if(s=i.c)i=o.m[t=s][e=i.m];if(!i)throw Error(r);return i.exports||i(i,i.exports={},function(n){return o("."!=n.charAt(0)?n:e+"/../"+n,t)}),i.exports};
+require.m = [];
+require.m[0] = { "index.js": function(module, exports, require){'use strict';
+
+$('main').on('tap', '.challenge .stats, .challenge .close', function() {
+	$(this).closest('.challenge').toggleClass('is-details');
+});}};
+undefined = require('index.js');
+}());
