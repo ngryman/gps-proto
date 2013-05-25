b71f96e4d896e2578e53de877b8060f9ea39aebc
Tuizi <fabien.rogeret@gmail.com>
2013-05-25 16:08:11 -0400

    Challenge type image

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,3 +26,15 @@ $('main').on('tap', '.challenge .stats, .challenge .close', function() {
     });
 })();
 
+$.Finger.flickDuration = 500;
+$('.challenge').on('flick', '.proof-image', function(e) {
+	var $selected = $('.is-selected', this),
+		$newSelected;
+
+	$newSelected = e.direction < 0 ? $selected.next('img') : $selected.prev('img');
+
+	if ($newSelected.length) {
+		$selected.removeClass('is-selected');
+		$newSelected.addClass('is-selected');
+	}
+});
